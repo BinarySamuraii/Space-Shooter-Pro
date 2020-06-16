399a79e2403f2d6f5b180e67c9cb3a1451f2f6fb
BinarySamurai <kenneth-m-buschman@comcast.net>
2020-06-16 00:09:14 -0400

    Added Player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    void Start()
+    { 
+        //take current position = new position (0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
