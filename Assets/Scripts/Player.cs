6afd657fdc8b7f528c1f34a9f5bf90ef15eb2c9b
BinarySamurai <kenneth-m-buschman@comcast.net>
2020-06-16 09:43:50 -0400

    Finished Player Scripting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _speed = 3.5f;
     
     void Start()
     { 
-        //take current position = new position (0,0,0)
+        //Player Start Position
         transform.position = new Vector3(0, 0, 0);
     }
 
     
     void Update()
     {
-        
+        PlayerMovement();
+        PlayerBoundaries();
+    }
+
+    void PlayerMovement()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * (_speed * Time.deltaTime));
+    }
+
+    void PlayerBoundaries()
+    {
+        var position = transform.position;
+        position = new Vector3(position.x, Mathf.Clamp(position.y, -3.8f, 0), 0);
+        transform.position = position;
+
+        if (transform.position.x >= 11.2f)
+        {
+            transform.position = new Vector3(-11f, transform.position.y, 0);
+        }else if (transform.position.x <= -11f)
+        {
+            transform.position = new Vector3(11f, transform.position.y, 0);
+        }
     }
 }
